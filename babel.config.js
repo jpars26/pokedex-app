831ac1355a02876d1ef4cd3ff68aa7c9bec92d3e
jpars26 <jpars131@gmail.com>
2025-08-07 13:09:22 -0300

    fix-babel-plugin

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-  ],
+// babel.config.js
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      'babel-preset-expo'     // <— este é o preset que o Expo usa por padrão
+    ],
+    plugins: [
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }]
+    ],
+  };
 };
